c93366b0d48c4a127da24784de0b2f26e7da5076
NaveenMare <naveenmareftw@gmail.com>
2023-07-09 23:51:13 +0530

    Added exception handling

--- MontyHall.BusinessLogic/GameBL.cs
+++ MontyHall.BusinessLogic/GameBL.cs
@@ -1,7 +1,7 @@
 ﻿using MontyHall.DataAccess;
 using MontyHall.DataAccess.Models;
 using MontyHall.Models;
-using System;
+using System.Collections.Generic;
 
 namespace MontyHall.BusinessLogic
 {
@@ -10,43 +10,58 @@ namespace MontyHall.BusinessLogic
         public GameCardModel CreateGame(CreateGameModel newGame)
         {
             int newGameId;
-            using(DataAdapter dataAdapter = new DataAdapter())
+            GameCardModel gameCard = new GameCardModel();
+            try
             {
-                List<GameData> list = new List<GameData>();
-
-                newGameId = dataAdapter.GameDataGenericRepository.GetAll().Select(x => x.GameId).DefaultIfEmpty(0).Max() + 1;
-
-                for (int i = 0; i< newGame.NoOfSimulations; i++)
+                using (DataAdapter dataAdapter = new DataAdapter())
                 {
-                    GameData gameData = new GameData()
-                    {
-                        GameId = newGameId,
-                        SwitchDoor = newGame.SwitchDoor,
-                    };
-                    list.Add(gameData);
-                }
-                dataAdapter.GameDataGenericRepository.AddRange(list);
-                dataAdapter.Save();                
-            }
+                    List<GameData> list = new List<GameData>();
 
-            return GetGameCard(newGameId);
+                    newGameId = dataAdapter.GameDataGenericRepository.GetAll().Select(x => x.GameId).DefaultIfEmpty(0).Max() + 1;
+
+                    for (int i = 0; i < newGame.NoOfSimulations; i++)
+                    {
+                        GameData gameData = new GameData()
+                        {
+                            GameId = newGameId,
+                            SwitchDoor = newGame.SwitchDoor,
+                        };
+                        list.Add(gameData);
+                    }
+                    dataAdapter.GameDataGenericRepository.AddRange(list);
+                    dataAdapter.Save();
+                }
+                gameCard = GetGameCard(newGameId);
+            }
+            catch (Exception e)
+            {
+
+            }
+            return gameCard;
         }
 
         public List<GameCardModel> GetGameCards()
         {
             List<GameCardModel> results = new List<GameCardModel>();
-            using (DataAdapter dataAdapter = new DataAdapter())
+            try
             {
-                var gameData = dataAdapter.GameDataGenericRepository.GetAll();
+                using (DataAdapter dataAdapter = new DataAdapter())
+                {
+                    var gameData = dataAdapter.GameDataGenericRepository.GetAll();
 
-                results = gameData.GroupBy(g => g.GameId)
-                                             .Select(g => new GameCardModel
-                                             {
-                                                 GameId = g.Key,
-                                                 SwitchDoor = g.First().SwitchDoor,
-                                                 NoOfSimulations = g.Count()
-                                             })
-                                             .ToList();
+                    results = gameData.GroupBy(g => g.GameId)
+                                                 .Select(g => new GameCardModel
+                                                 {
+                                                     GameId = g.Key,
+                                                     SwitchDoor = g.First().SwitchDoor,
+                                                     NoOfSimulations = g.Count()
+                                                 })
+                                                 .ToList();
+                }
+            }
+            catch(Exception e)
+            {
+
             }
             return results;
         }
@@ -54,18 +69,25 @@ namespace MontyHall.BusinessLogic
         public GameCardModel GetGameCard(int cardId)
         {
             List<GameCardModel> results = new List<GameCardModel>();
-            using (DataAdapter dataAdapter = new DataAdapter())
+            try
             {
-                var gameData = dataAdapter.GameDataGenericRepository.GetByValues(x=> x.GameId == cardId);
+                using (DataAdapter dataAdapter = new DataAdapter())
+                {
+                    var gameData = dataAdapter.GameDataGenericRepository.GetByValues(x => x.GameId == cardId);
 
-                results = gameData.GroupBy(g => g.GameId)
-                                             .Select(g => new GameCardModel
-                                             {
-                                                 GameId = g.Key,
-                                                 SwitchDoor = g.First().SwitchDoor,
-                                                 NoOfSimulations = g.Count()
-                                             })
-                                             .ToList();
+                    results = gameData.GroupBy(g => g.GameId)
+                                                 .Select(g => new GameCardModel
+                                                 {
+                                                     GameId = g.Key,
+                                                     SwitchDoor = g.First().SwitchDoor,
+                                                     NoOfSimulations = g.Count()
+                                                 })
+                                                 .ToList();
+                }
+            }
+            catch(Exception e)
+            {
+
             }
             return results[0];
         }
@@ -73,41 +95,55 @@ namespace MontyHall.BusinessLogic
         public List<GameData> GetGameData(int GameId)
         {
             List<GameData> results = new List<GameData>();
-            using (DataAdapter dataAdapter = new DataAdapter())
+            try
             {
-                 results = dataAdapter.GameDataGenericRepository.GetByValues(x=>x.GameId == GameId);
+                using (DataAdapter dataAdapter = new DataAdapter())
+                {
+                    results = dataAdapter.GameDataGenericRepository.GetByValues(x => x.GameId == GameId);
+                }
+            }
+            catch(Exception e)
+            {
+
             }
             return results;
         }
 
         public List<GameData> RunGame(int gameId)
         {
-            using (DataAdapter dataAdapter = new DataAdapter())
+            List<GameData> results = new List<GameData>();
+            try
             {
-                List<GameData> gameDataList = dataAdapter.GameDataGenericRepository.GetByValues(x => x.GameId == gameId);
-
-                foreach(GameData gameData in gameDataList)
+                using (DataAdapter dataAdapter = new DataAdapter())
                 {
-                    Random randomNumber = new Random();
-                    int doorId = randomNumber.Next(1,4);
-                    int winningDoor = randomNumber.Next(1,4);
-                    int revealDoor = GetRevealedDoor(winningDoor, doorId);
+                    List<GameData> gameDataList = dataAdapter.GameDataGenericRepository.GetByValues(x => x.GameId == gameId);
 
-                    gameData.WinDoorNumber = winningDoor;
-                    gameData.SelectedDoorNumber = doorId;
-
-                    gameData.RevealDoorNumber = revealDoor;
-
-                    if (gameData.SwitchDoor == true)
+                    foreach (GameData gameData in gameDataList)
                     {
-                        gameData.SelectedDoorNumber = GetSwitchedDoor(doorId, revealDoor);
+                        Random randomNumber = new Random();
+                        int doorId = randomNumber.Next(1, 4);
+                        int winningDoor = randomNumber.Next(1, 4);
+                        int revealDoor = GetRevealedDoor(winningDoor, doorId);
+
+                        gameData.WinDoorNumber = winningDoor;
+                        gameData.SelectedDoorNumber = doorId;
+
+                        gameData.RevealDoorNumber = revealDoor;
+
+                        if (gameData.SwitchDoor == true)
+                        {
+                            gameData.SelectedDoorNumber = GetSwitchedDoor(doorId, revealDoor);
+                        }
                     }
+                    dataAdapter.Save();
                 }
-
-                dataAdapter.Save();
+                results = GetGameData(gameId);
             }
+            catch(Exception e)
+            {
 
-            return GetGameData(gameId);
+            }
+            return results;
         }
 
         private static int GetRevealedDoor(int winningDoor, int selectedDoor)
@@ -116,7 +152,7 @@ namespace MontyHall.BusinessLogic
             Random random = new Random();
             do
             {
-                revealedDoor = random.Next(1,4);
+                revealedDoor = random.Next(1, 4);
             } while (revealedDoor == winningDoor || revealedDoor == selectedDoor);
 
             return revealedDoor;
@@ -128,7 +164,7 @@ namespace MontyHall.BusinessLogic
             Random random = new Random();
             do
             {
-                switchedDoor = random.Next(1,4);
+                switchedDoor = random.Next(1, 4);
             } while (switchedDoor == selectedDoor || switchedDoor == revealedDoor);
 
             return switchedDoor;
